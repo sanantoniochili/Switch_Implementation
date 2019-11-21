2332f039237e79219b9d854d2a7d28550d5fd178
Antonia Tsili <to-tsi@windowslive.com>
2019-11-21 00:35:47 +0000

    Plots for all methods

--- read_results.py
+++ read_results.py
@@ -48,7 +48,53 @@ def autolabel(rects):
 
 ''' Process results '''
 
-''' No. of optimised structures per method '''
+# ''' No. of optimised structures per method (same no. of total structures) '''
+
+# ran_succ_list = []
+# rat_succ_list = []
+
+# df = pd.read_csv('results_test.csv', skipinitialspace=True)
+# labels = list(df['method'].unique())
+# dicts = {}
+
+# for method in labels:
+#     random = df[df['structure'].str.contains(
+#         "rat") == False].loc[df['method'] == method]
+#     rattled = df[df['structure'].str.contains(
+#         "rat")].loc[df['method'] == method]
+
+#     ran_succ = len([b for b in random['opt_succ'] if b])
+#     rat_succ = len([b for b in rattled['opt_succ'] if b])
+
+#     ran_succ_list.append(ran_succ)
+#     rat_succ_list.append(rat_succ)
+
+# total = len(df.loc[df['method'] == method]) / 2
+
+# x = np.arange(len(labels))  # the label locations
+# width = 0.35  # the width of the bars
+
+# fig, ax = plt.subplots()
+# ax.set_ylim(0,total+10) # set total samples as y limit (add space for view)
+# rects1 = ax.bar(x - width/2, ran_succ_list, width, label='Random')
+# rects2 = ax.bar(x + width/2, rat_succ_list, width, label='Rattled')
+
+# # Add some text for labels, title and custom x-axis tick labels, etc.
+# ax.set_ylabel('Structures')
+# ax.set_title('Successfully optimised structures')
+# ax.set_xticks(x)
+# ax.set_xticklabels(labels)
+# ax.legend()
+
+# autolabel(rects1)
+# autolabel(rects2)
+
+# fig.tight_layout()
+
+# plt.show()
+
+
+''' No. of optimised structures per method (diff no. of total structures) '''
 
 ran_succ_list = []
 rat_succ_list = []
@@ -71,23 +117,29 @@ for method in labels:
 
 total = len(df.loc[df['method'] == method]) / 2
 
-x = np.arange(len(labels))  # the label locations
+y = np.arange(1)  # the label locations
 width = 0.35  # the width of the bars
 
-fig, ax = plt.subplots()
-ax.set_ylim(0,total+10) # set total samples as y limit (add space for view)
-rects1 = ax.bar(x - width/2, ran_succ_list, width, label='Random')
-rects2 = ax.bar(x + width/2, rat_succ_list, width, label='Rattled')
+fig, axs = plt.subplots(len(labels))
+fig.suptitle('Successfully optimised structures')
 
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Structures')
-ax.set_title('Successfully optimised structures')
-ax.set_xticks(x)
-ax.set_xticklabels(labels)
-ax.legend()
+count = 0
+for ax in axs:
+	# ax.set_ylim(0,total+10) # set total samples as y limit (add space for view)
+	rects1 = ax.barh(y - width/2, ran_succ_list[count], width, label='Random')
+	rects2 = ax.barh(y + width/2, rat_succ_list[count], width, label='Rattled')
 
-autolabel(rects1)
-autolabel(rects2)
+	# Add some text for labels, title and custom x-axis tick labels, etc.
+	# ax.set_xlabel(labels[count])
+	ax.set_yticks(y)
+	ax.set_yticklabels([labels[count]])
+	ax.invert_yaxis()  # labels read top-to-bottom
+	ax.legend()
+
+	# autolabel(rects1)
+	# autolabel(rects2)
+
+	count += 1
 
 # fig.tight_layout()
 
