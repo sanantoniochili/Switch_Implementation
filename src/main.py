441327757e55280f12f52739711367b7e00a1345
sanantoniochili <to-tsi@windowslive.com>
2020-01-24 16:47:15 +0000

    Start CG code

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
-from sympy import *
-import numpy as np
+# import torch
+import sys
+import argparse
+import fileinput
+from ase import *
+from ase.io import read as aread
 
-x, y = symbols('x y')
-A, B, C = symbols('A B C')
-q_1, q_2, p, e = symbols('q_1 q_2 p e')
+# x = torch.ones(2, 2, requires_grad=True)
 
+# f = A*(exp(-B*sqrt(x**2 + y**2))) - C/((x**2 + y**2)**3) + \
+#     (q_1*q_2/(4*p*e)) * (1/sqrt(x**2 + y**2))
+# print(y)
 
-f = A*(exp(-B*sqrt(x**2 + y**2))) - C/((x**2 + y**2)**3) + \
-    (q_1*q_2/(4*p*e)) * (1/sqrt(x**2 + y**2))
+# # vector-Jacobian product
+# v = torch.tensor([[0.1, 1.0],[1, 0.0001]], dtype=torch.float)
+# y.backward(v)
 
-deriv = f.diff(x)
-print(deriv.evalf(subs={x: 1, y: 1}))
+Atoms = aread("/users/phd/tonyts/Desktop/Data/RandomStart_Sr3Ti3O9/1.cif")
+print(Atoms)
