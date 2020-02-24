f5c550fafb4607533cbb419e78d3d05d442c2ab8
sanantoniochili <to-tsi@windowslive.com>
2020-02-24 18:25:23 +0000

    Update potential

--- src/main.py
+++ src/main.py
@@ -18,23 +18,34 @@ charges = {
 	'Sr': 2.,
 	'Ti': 4.}
 
-class Buckingham_Coulomb:
-	def __init__(self, charge_dict):
-		self.charges = charge_dict
-
-	def set_parameters(self, alpha, epsilon, \
-							real_cut_off=4, recip_cut_off=4):
+class Coulomb:
+	def __init__(self, alpha, real_cut_off=4, recip_cut_off=4):
 		self.real_cut_off  = real_cut_off
 		self.recip_cut_off = recip_cut_off
-		self.epsilon       = epsilon
 		self.alpha         = alpha
 
-	def set_structure(self, atoms):
-		self.atoms = atoms
+	def set_structure(self, atoms, volume, vects, charge_dict):
+		'''
+		 atoms:       ASE object
+		 vects:       Supercell vectors
+		 pos:         ASE positions of atoms
+		 charge_dict: Dictionary of chemical symbols with charge value
+		'''
+		self.atoms   = atoms
+		self.vects   = vects
+		self.pos     = atoms.get_scaled_positions()@vects
+		self.volume  = volume
+		self.charges = [library[x] for x in atoms.get_chemical_symbols()]
 
-	# def get_structure_vects(self):
-		# pass
-		# return vects
+	def get_reciprocal_vects(self):
+		'''
+		 Calculate reciprocal vectors
+		'''
+		rvects = np.zeros((3,3))
+		for i in np.arange(3):
+			rvects[i,] = 2*math.pi*np.cross( self.vects[(1+i)%3,], \
+									self.vects[(2+i)%3] ) / self.volume
+			return rvects
 
 	def get_shifts(self, cut_off, vects):
 		''' 
@@ -54,29 +65,87 @@ class Buckingham_Coulomb:
 		shifts = shifts@vects
 		return shifts
 
-	def calc_real(self, vects):
+	def get_charges_mult(self, index1, index2):
+		'''
+		 Find respective charges and
+		 return their product
+		'''
+		return ( self.charges[index1]*self.charges[index2] )
+
+	# def calc_self(self, esum):
+		'''
+		 Calculate self interaction term
+		'''
+	#     for i in np.arange(N):
+	#         esum[i, i] = esum[i, i]-alpha/math.sqrt(math.pi)
+
+	def calc_real(self, vects, esum):
 		'''
 		 Calculate short range
 		'''
-		esum = np.zeros((N,N)) 
+		shifts = get_shifts( self.real_cut_off, \
+											self.vects )
 
 		for atomi in range(0,N):
 			for atomj in range(atomi, N):
 
 				if atomi != atomj: # skip in case it's the same atom
-					rij = np.linalg.norm(pos[i,] - pos[j,])
-		            esum[i, j] += math.erfc( self.alpha*rij )/(2*rij)
+					rij = np.linalg.norm(self.pos[atomi,] - self.pos[atomj,])
+					esum[atomi, atomj] += ( get_charges_mult(atomi,atomj) ) * \
+												math.erfc( self.alpha*rij )/(2*rij)
 
-		        # take care of the rest lattice (+ Ln)
-		        for shift in get_shifts( self.real_cut_off, \
-		        							self.get_structure_vects ):
-		        	rij = np.linalg.norm(pos[i,] + shift - pos[j,])
-		            esum[i, j] += math.erfc( self.alpha*rij )/(2*rij)
+				# take care of the rest lattice (+ Ln)
+				for shift in shifts:
+					rij = np.linalg.norm(self.pos[atomi,] + shift - self.pos[atomj,])
+					esum[atomi, atomj] += ( get_charges_mult(atomi,atomj) ) * \
+												math.erfc( self.alpha*rij )/(2*rij)
 
-pot = Buckingham_Coulomb(charges)
-pot.calc_real()
+	def calc_recip(self, recip_vects, esum):
+		'''
+		 Calculate long range
+		'''
+		shifts = get_shifts( self.recip_cut_off, \
+									self.get_reciprocal_vects )
+		for atomi in range(0,N):
+			for atomj in range(atomi,N):
 
-# Atoms = aread("/users/phd/tonyts/Desktop/Data/RandomStart_Sr3Ti3O9/1.cif")
+				dist = self.pos[atomi,] - self.pos[atomj,]
+
+				for s in range(0,shifts):
+					k = shifts[s,]
+
+					# -k^2/4a^2
+					po = -np.dot(k,k)/(4*alpha**2)
+					# 4pi^2 * e^(power) * cos(k(ri-rj))
+					numerator = (4*math.pi**2) * math.exp(po) * math.cos(np.dot(k, dist))
+					# k^2 * 2piV
+					denominator = np.dot(k,k) * (2*math.pi*cell_volume)
+
+					esum[atomi, atomj] += (( get_charges_mult(atomi,atomj) ) * \
+														(numerator/denominator))
+	
+	def calc_complete(self, esum):
+		'''
+		 Complete calculations
+		'''
+
+		# Unit conversion
+		esum = esum*14.399645351950543
+
+		# symmetry completion
+		for atomi in range(0,N):
+			for atomj in range(0,i):
+				esum[atomi, atomj] = esum[atomj, atomi]
+
+
+
+
+Atoms = aread("/users/phd/tonyts/Desktop/Data/RandomStart_Sr3Ti3O9/1.cif")
+
+
+volume = abs(np.linalg.det(vects))
+pot = Coulomb(2/(volume**(1.0/3)))
+
 # charges = [library[x] for x in Atoms.get_chemical_symbols()]
 # Atoms.set_initial_charges(charges)
 
