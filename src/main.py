5bb707ffb9372a36a28b96b4c0a2a04843d799fc
Antonia Tsili <to-tsi@windowslive.com>
2020-03-05 01:15:54 +0200

    Movingto Buckingham

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ from ase.io import read as aread
 from ase.geometry import Cell
 
 
-DATAPATH = "/users/phd/tonyts/Desktop/Data/"
+DATAPATH = "../../Data/"
 
 
 charge_dict = {
@@ -22,38 +22,23 @@ charge_dict = {
 	'Sr': 2.,
 	'Ti': 4.}
 
-class Coulomb:
-	'''
-	 Calculations for the Coulomb energy contribution.
-	 Ewald summation method used for long range.
-	'''
-	def __init__(self, alpha, real_cut_off=4, recip_cut_off=4):
-		self.real_cut_off  = real_cut_off
-		self.recip_cut_off = recip_cut_off
-		self.alpha         = alpha
+class Potential:
+	def __init__(self):
+		pass
 
-	def set_structure(self, charge_dict, positions, vects, volume):
+	def set_structure(self, charge_dict, atoms):
 		'''
 		 atoms:       ASE object
-		 vects:       Supercell vectors
-		 pos:         ASE positions of atoms
+		 vects:       Unit cell vectors
+		 positions:   ASE positions of atoms
 		 charge_dict: Dictionary of chemical symbols with charge value
 		'''
-		self.vects   = vects
-		self.pos     = positions
-		self.volume  = volume
+		self.vects   = atoms.get_cell()
+		self.pos     = atoms.get_positions()
+		self.volume  = abs(np.linalg.det(self.vects))
 		self.charges = [charge_dict[x] for x in atoms.get_chemical_symbols()]
-		self.N       = len(positions)
-
-	def get_reciprocal_vects(self):
-		'''
-		 Calculate reciprocal vectors
-		'''
-		rvects = np.zeros((3,3))
-		for i in np.arange(3):
-			rvects[i,] = 2*pi*np.cross( self.vects[(1+i)%3,], \
-									self.vects[(2+i)%3] ) / self.volume
-		return rvects
+		self.N       = len(self.pos)
+		self.atoms   = atoms
 
 	def get_shifts(self, cut_off, vects):
 		''' 
@@ -72,6 +57,27 @@ class Coulomb:
 				i = i+1
 		shifts = shifts@vects
 		return shifts
+
+
+class Coulomb(Potential):
+	'''
+	 Calculations for the Coulomb energy contribution.
+	 Ewald summation method used for long range.
+	'''
+	def __init__(self, alpha, real_cut_off=4, recip_cut_off=4):
+		self.real_cut_off  = real_cut_off
+		self.recip_cut_off = recip_cut_off
+		self.alpha         = alpha
+
+	def get_reciprocal_vects(self):
+		'''
+		 Calculate reciprocal vectors
+		'''
+		rvects = np.zeros((3,3))
+		for i in np.arange(3):
+			rvects[i,] = 2*pi*np.cross( self.vects[(1+i)%3,], \
+									self.vects[(2+i)%3] ) / self.volume
+		return rvects
 
 	def get_charges_mult(self, index1, index2):
 		'''
@@ -97,18 +103,16 @@ class Coulomb:
 		if esum == []:
 			esum = np.zeros((Cpot.N, Cpot.N))
 		shifts = self.get_shifts( self.real_cut_off,self.vects )
-		for atomi in range(0, self.N): 
-			for atomj in range(atomi, self.N): 
-
-				if atomi != atomj: # skip in case it's the same atom
-					rij = np.linalg.norm(self.pos[atomi,] - self.pos[atomj,])
-					esum[atomi, atomj] += ( self.get_charges_mult(atomi,atomj) * \
+		for ioni in range(0, self.N): 
+			for ionj in range(ioni, self.N): 
+				if ioni != ionj: # skip in case it's the same atom in original unit cell
+					rij = np.linalg.norm(self.pos[ioni,] - self.pos[ionj,])
+					esum[ioni, ionj] += ( self.get_charges_mult(ioni,ionj) * \
 												 math.erfc( self.alpha*rij )/(2*rij) )
-
-					# take care of the rest lattice (+ Ln)
-				for shift in shifts: # !!!!!!! MAYBE NEEDS TO BE OUTSIDE if BLOCK !!!!!!!
-					rij = np.linalg.norm(self.pos[atomi,] + shift - self.pos[atomj,])
-					esum[atomi, atomj] += ( self.get_charges_mult(atomi,atomj) * \
+				# take care of the rest lattice (+ Ln)
+				for shift in shifts:
+					rij = np.linalg.norm(self.pos[ioni,] + shift - self.pos[ionj,])
+					esum[ioni, ionj] += ( self.get_charges_mult(ioni,ionj) * \
 												math.erfc( self.alpha*rij )/(2*rij) )
 		return esum
 
@@ -119,21 +123,16 @@ class Coulomb:
 		if esum == []:
 			esum        = np.zeros((Cpot.N, Cpot.N))
 		shifts = self.get_shifts( self.recip_cut_off,recip_vects )
-		for atomi in range(0, self.N): 
-			for atomj in range(atomi, self.N): 
+		for ioni in range(0, self.N): 
+			for ionj in range(ioni, self.N): 
 
-				dist = self.pos[atomj,] - self.pos[atomi,] # order of atomi, atomj matters
+				dist = self.pos[ionj,] - self.pos[ioni,] 
 				for k in shifts:
 					po = -np.dot(k,k)/(4*alpha**2)
 					numerator = 4 * (pi**2) * (math.exp(po)) * math.cos(np.dot(k, dist))
 					denominator = np.dot(k,k) * 2 * pi * self.volume
-					esum[atomi, atomj] += (( self.get_charges_mult(atomi,atomj) ) * \
+					esum[ioni, ionj] += (( self.get_charges_mult(ioni,ionj) ) * \
 																(numerator/denominator))
-					# term = (4*math.pi**2)/np.dot(k,k)
-					# term = term*math.exp(-np.dot(k,k)/(4*alpha**2))
-					# v = self.pos[atomj,] - self.pos[atomi,]
-					# term = term*math.cos(np.dot(k, v))
-					# esum[atomi, atomj] += term/(2*math.pi*self.volume)
 		return esum
 
 	def calc_complete(self, esum):
@@ -141,50 +140,87 @@ class Coulomb:
 		 Complete lower triangular matrix of monopole to monopole
 		'''
 		esum *= 14.399645351950543
-		for atomi in range(0, self.N):
-			for atomj in range(0, atomi):
-				esum[atomi, atomj] = esum[atomj, atomi]
+		for ioni in range(0, self.N):
+			for ionj in range(0, ioni):
+				esum[ioni, ionj] = esum[ionj, ioni]
 		return esum
 
+class Buckingham(Potential):
+	'''
+	 Calculations for the Buckingham energy contribution.
+	'''
+	def __init__(self, filename, real_cut_off=2):
+		'''
+		 Set atom_i-atom_j parameters as read from library file:
+		 - par: [A(eV), rho(Angstrom), C(eVAngstrom^6)]
+		 - lo : min radius (Angstrom)
+		 - hi : max radius (Angstrom)
+		'''
+		self.buck = {}
+		self.real_cut_off  = real_cut_off
 
-# class Buckingham:
-# 	'''
-# 	 Calculations for the Buckingham energy contribution.
-# 	'''
-# 	def __init__(self, filename):
-# 		'''
-# 		 Set atom_i-atom_j parameters as read from library file:
-# 		 - par: [A(eV), rho(Angstrom), C(eVAngstrom^6)]
-# 		 - lo : min radius (Angstrom)
-# 		 - hi : max radius (Angstrom)
-# 		'''
-# 		self.buck = {}
+		try:
+			with open(filename,"r") as fin:
+				for line in fin:
+					line = line.split()
+					if ( len(line)<4 ):
+						continue
+					pair = (min(line[0],line[2]), max(line[0],line[2]))
+					self.buck[pair] = {}
+					self.buck[pair]['par'] = list(map(float, line[4:7]))
+					self.buck[pair]['lo'] = float(line[7])
+					self.buck[pair]['hi'] = float(line[-1])
+		except IOError:
+			print("No library file found.")
 
-# 		try:
-# 			with open(filename,"r") as fin:
-# 				for line in fin:
-# 					line = line.split()
-# 					if ( len(line)<4 ):
-# 						continue
-# 					pair = (min(line[0],line[2]), max(line[0],line[2]))
-# 					self.buck[pair] = {}
-# 					self.buck[pair]['par'] = list(map(float, line[4:7]))
-# 					self.buck[pair]['lo'] = float(line[7])
-# 					self.buck[pair]['hi'] = float(line[-1])
-# 		except IOError:
-# 			print("No library file found.")
+	def calc_real(self, esum=[]):
+		'''
+		 Interatomic potential for original unit cell
+		'''
+		if esum == []:
+			esum        = np.zeros((Cpot.N, Cpot.N))
+		chemical_symbols = self.atoms.get_chemical_symbols()
+		shifts = self.get_shifts( self.real_cut_off,self.vects )
+		for ioni in range(self.N):
+			for ionj in range(ioni+1, self.N):
+				# Find the pair we are examining
+				pair = (min(chemical_symbols[ioni], chemical_symbols[ionj]), \
+								max(chemical_symbols[ioni], chemical_symbols[ionj]))
+				if (pair in self.buck):
+				# Pair of ions is listed in parameters file
+					A    = self.buck[pair]['par'][0]
+					rho  = self.buck[pair]['par'][1]
+					C    = self.buck[pair]['par'][2]
 
+					dist = np.linalg.norm(self.pos[ioni] - self.pos[ionj])
+					if (dist <= self.buck[pair]['hi']):
+					# Check if distance of ions allows interaction 					
+						esum[ioni, ionj] += A*math.exp(-1.0*dist/rho) - C/dist**6
+					for shift in shifts: # Interaction with rest of cells
+						dist = np.linalg.norm(self.pos[ioni] + shift - self.pos[ionj])
+						# Check if distance of ions allows interaction 					
+						if (dist <= self.buck[pair]['hi']):
+							esum[ioni, ionj] += A*math.exp(-1.0*dist/rho) - C/dist**6
+		return esum
+
+	def calc_complete(self, esum):
+		'''
+		 Complete lower triangular matrix of monopole to monopole
+		'''
+		for ioni in range(0, self.N):
+			for ionj in range(0, ioni+1):
+				esum[ioni, ionj] = esum[ionj, ioni]
+		return esum
 
 if __name__=="__main__":
-	atoms  = aread("../../Data/RandomStart_Sr3Ti3O9/1.cif")
+	atoms  = aread(DATAPATH+"RandomStart_Sr3Ti3O9/3.cif")
 	vects  = np.array(atoms.get_cell())
-	print(vects)
 
 	volume = abs(np.linalg.det(vects))
 	alpha  = 2/(volume**(1.0/3))
 
-	Cpot        = Coulomb(alpha)
-	Cpot.set_structure(charge_dict, atoms.get_positions(), vects, volume)
+	Cpot        = Coulomb(alpha,4,4)
+	Cpot.set_structure(charge_dict, atoms)
 	rvects      = Cpot.get_reciprocal_vects()
 
 	Er  = Cpot.calc_real()
@@ -201,10 +237,17 @@ if __name__=="__main__":
 	print("Recip:\t"+str(sum(sum(Cpot.calc_complete(Erc)))))
 	print("Total:\t"+str(sum(sum(Etotal))))
 
-	# filename    = DATAPATH+"Libraries/buck.lib"
-	# Bpot 		= Buckingham(filename)
+	print("------------------")
+
+	filename    = DATAPATH+"Libraries/buck.lib"
+	Bpot 		= Buckingham(filename)
+	Bpot.set_structure(charge_dict, atoms)
+
+	Einter = Bpot.calc_real()
+	print(sum(sum(Bpot.calc_complete(Einter))))
+
+	# print("Inter:\t"+str(Einter))
+	# print(sum(sum(Einter)))
 
 # https://github.com/SINGROUP/Pysic/blob/master/fortran/Geometry.f90
 # https://github.com/vlgusev/IPCSP/blob/master/tools/matrix_generator.py?
-
-	from matrix_generator import QEwald
