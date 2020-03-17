bb746ce54837f2e2a5f297319f1d1660488f55b7
sanantoniochili <sanantoniochili@github.com>
2020-03-17 14:04:03 +0000

    Save all progress

--- simple_run.sh
+++ simple_run.sh
@@ -5,26 +5,26 @@ read -p "Choose test environment: " testdir
 
 # Find input folder in every method folder
 for method in `ls -d -- $testdir/*/`; do
-	if [ -d "${method}/input" ]; then
+	if [ -d "${method}input" ]; then
 		# Structures' folders e.g. "random"
-		for d in `ls ${method}/input`; do
-			for file in `ls ${method}/input/$d`; do
+		for d in `ls ${method}input`; do
+			for file in `ls ${method}input/$d`; do
 
 				# Create missing output folders in same hierarchy
-				if [ ! -d "${method}/output" ]; then
+				if [ ! -d "${method}output" ]; then
 					echo "Creating ouput directory.."
-				    mkdir ${method}/output
+				    mkdir ${method}output
 				fi
-				if [ ! -d "${method}/output/$d" ]; then
+				if [ ! -d "${method}output/$d" ]; then
 					echo "Creating ${d} directory.."
-				    mkdir ${method}/output
+				    mkdir ${method}output/${d}
 				fi
 
 				# Get structure name
 				struct="$(cut -d'.' -f1 <<< $file )"
 
 				# Run GULP
-				gulp < ${method}/input/$d/file > ${method}/output/$d/${struct}.got
+				gulp < ${method}input/$d/${file} > ${method}output/$d/${struct}.got
 				
 			done
 		done
