f8ce2f728e484b6913081e73eee20dbaaa50d217
Antonia Tsili <to-tsi@windowslive.com>
2020-04-14 14:00:29 +0100

    Changing python scripts

---
+++ calculate_energy.py
@@ -0,0 +1,178 @@
+import sys
+import argparse
+import fileinput
+import numpy as np
+import pandas as pd
+
+from cmath import pi
+from cmath import exp
+import cmath
+import math
+
+from ase import *
+from ase.io import read as aread
+
+from potential import *
+
+DATAPATH = "../../Data/"
+
+charge_dict = {
+	'O' : -2.,
+	'Sr':  2.,
+	'Ti':  4.,
+	'Cl': -1.,
+	'Na':  1.
+}
+
+atom_types = {
+	'O' : 1,
+	'Sr': 2,
+	'Ti': 3
+}
+
+import shutil
+if __name__=="__main__":
+	columns = shutil.get_terminal_size().columns
+	parser = argparse.ArgumentParser(
+		description='Define input')
+	parser.add_argument(
+		'filename', metavar='--input', type=str,
+		help='.cif file to read')
+	args = parser.parse_args()
+	atoms = aread(args.filename)
+
+
+	############################################################################################
+	########################################### GULP ###########################################
+	############################################################################################
+
+	# from ase.calculators.gulp import GULP
+	# calc = GULP(keywords='conp full nosymm opti unfix', \
+	# 				library='buck.lib')
+	# atoms.set_calculator(calc)
+	# print(atoms.get_potential_energy())
+
+	############################################################################################
+	###################################### INITIALISATION ######################################
+	############################################################################################
+
+	vects    = np.array(atoms.get_cell())
+	volume   = abs(np.linalg.det(vects))
+	N        = len(atoms.get_positions())
+	accuracy = 0.00001 # Demanded accuracy of terms (tolerance of parameters?)
+	alpha    = N**(1/6) * pi**(1/2) / volume**(1/3)
+	LOG      = "src/test.log"
+
+	#########################################################################################
+	######################################## COULOMB ########################################
+	#########################################################################################
+	
+	print("\n")
+	print("==========================COULOMB==========================".center(columns))
+
+	real_cut  = (-np.log(accuracy))**(1/2)/alpha
+	recip_cut = 2*alpha*(-np.log(accuracy))**(1/2)
+	# Cpot      = Coulomb(alpha,real_cut_off=real_cut,recip_cut_off=recip_cut)
+	# Cpot.set_structure(charge_dict, atoms)
+	Cpot      = Coulomb(charge_dict, atoms)
+	Cpot.set_parameters(alpha,real_cut_off=real_cut,recip_cut_off=recip_cut)
+
+	rvects    = Cpot.get_reciprocal_vects()
+
+	Er  = Cpot.calc_real()
+	Es  = Cpot.calc_self()
+	Erc = Cpot.calc_recip(rvects)
+	Eupper = Er + Es + Erc
+	Etotal = Cpot.calc_complete(Eupper)
+
+	print("--------------------------custom implementation---------------------------------".center(columns))
+	print("Real:\t\t"+str(sum(sum(Cpot.calc_complete(Er)))))
+	print("Self:\t\t"+str(sum(sum(Cpot.calc_complete(Es)))))
+	print("Recip:\t\t"+str(sum(sum(Cpot.calc_complete(Erc)))))
+	print("Electrostatic:\t"+str(sum(sum(Etotal))))
+	print("----------------------------------LAMMPS----------------------------------------".center(columns))
+
+	# Change atom_style parameter to allow charges
+	LAMMPSlib.default_parameters['lammps_header'] = ['units metal', 'atom_style charge', \
+														'atom_modify map array sort 0 0']
+
+	cmds = [
+			"set type 1 charge 2.0",  # Sr
+			"set type 2 charge -2.0", # O
+			"set type 3 charge 4.0",  # Ti
+			"pair_style coul/long "+str(real_cut),
+			"pair_coeff * *",
+			"kspace_style ewald "+str(accuracy)]   
+	lammps = LAMMPSlib(lmpcmds=cmds, atom_types=atom_types, log_file=LOG)
+	atoms.set_initial_charges(Cpot.charges)
+	atoms.set_calculator(lammps)
+
+	print("Electrostatic:\t"+str(atoms.get_potential_energy()))
+	print("--------------------------------------------------------------------------------".center(columns))
+
+	############################################################################################
+	######################################## BUCKINGHAM ########################################
+	############################################################################################
+
+	print("\n")
+	print("==========================BUCKINGHAM==========================".center(columns))
+
+	libfile    = DATAPATH+"Libraries/buck.lib"
+	Bpot 	   = Buckingham(charge_dict, atoms)
+	Bpot.set_parameters(libfile)
+	Einter = Bpot.calc_real()
+
+	print("--------------------------custom implementation---------------------------------".center(columns))
+	print("Interatomic:\t"+str(Einter))
+	print("----------------------------------LAMMPS----------------------------------------".center(columns))
+	
+	cmds = [
+			"pair_style buck 10",
+			"pair_coeff 1 2 1952.39 0.33685 19.22", # Sr-O
+			"pair_coeff 1 3 4590.7279 0.261 0.0",   # Ti-O
+			"pair_coeff 1 1 1388.77 0.36262 175",   # O-O
+			"pair_coeff 2 2 0.0 1.0 0.0",
+			"pair_coeff 3 3 0.0 1.0 0.0",
+			"pair_coeff 2 3 0.0 1.0 0.0"]   
+	lammps = LAMMPSlib(lmpcmds=cmds, atom_types=atom_types, log_file=LOG)
+	atoms.set_calculator(lammps)
+	
+	print("Interatomic:\t"+str(atoms.get_potential_energy()))
+	print("--------------------------------------------------------------------------------".center(columns))
+
+	############################################################################################
+	########################################### TOTAL ##########################################
+	############################################################################################
+
+	print("\n")
+	print("==========================TOTAL==========================".center(columns))
+	print("--------------------------custom implementation---------------------------------".center(columns))
+	print("Total lattice:\t"+str(sum(sum(Etotal)) + Einter))
+	print("--------------------------------------------------------------------------------".center(columns))
+
+	############################################################################################
+	########################################## OUTPUT ##########################################
+	############################################################################################
+
+	# ''' SAVE TO CSV '''
+	# ''' NAME '''
+	# count = args.filename.split('.')[-2].split('/')[-1]
+	# structure = 'structure'+count
+	# ''' FOLDER '''
+	# folder = 'random'
+	# ''' DATAFRAME '''
+	# df = pd.DataFrame.from_dict({'structure': [structure], 'folder': [folder], 'interatomic_custom': [Einter],\
+	# 							 'interatomic_lammps': [atoms.get_potential_energy()], 'electrostatic_custom': [sum(sum(Etotal))] }, orient='columns')
+	# fileout = "src/custom_all.csv"
+	# try:
+	# 	df_in = pd.read_csv(fileout) 
+	# 	if(df_in.empty):
+	# 		df.to_csv(fileout, mode='w', header=True)
+	# 	else:
+	# 		df.to_csv(fileout, mode='a', header=False)
+	# except:
+	# 	df.to_csv(fileout, mode='w', header=True)
+
+
+# https://github.com/SINGROUP/Pysic/blob/master/fortran/Geometry.f90
+# https://github.com/vlgusev/IPCSP/blob/master/tools/matrix_generator.py?
---
+++ potential.py
@@ -0,0 +1,204 @@
+from scipy.special import erfc
+import numpy as np
+
+from cmath import pi
+from cmath import exp
+import cmath
+import math
+
+from ase import *
+from ase.visualize import view
+from ase.geometry import Cell
+
+from ase.calculators.gulp import GULP
+from ase.calculators.lammpslib import LAMMPSlib
+
+class Potential:
+	def __init__(self, charge_dict, atoms):
+		'''
+		 atoms:       ASE object
+		 vects:       Unit cell vectors
+		 positions:   ASE positions of atoms
+		 charge_dict: Dictionary of chemical symbols with charge value
+		'''
+		self.vects   = atoms.get_cell()
+		self.pos     = atoms.get_positions()
+		self.volume  = abs(np.linalg.det(self.vects))
+		self.charges = [charge_dict[x] for x in atoms.get_chemical_symbols()]
+		self.N       = len(self.pos)
+		self.atoms   = atoms
+
+	def set_parameters(self):
+		pass
+
+	def get_shifts(self, cut_off, vects):
+		''' 
+		 Get all possible lattice positions:   
+		 (2cut_off+1)^3-{case of (cut_off,cut_off,cut_off)}
+		 combinations in R^3  
+		'''
+		shifts = np.zeros(((2*cut_off+1)**3 -1, 3))
+		tmp = np.array([cut_off,cut_off,cut_off])
+
+		i = 0
+		for shift in np.ndindex(2*cut_off+1, 2*cut_off+1, 2*cut_off+1):
+			if shift != (cut_off,cut_off,cut_off):
+				shifts[i,] = shift
+				shifts[i,] = shifts[i,] - tmp
+				i = i+1
+		shifts = shifts@vects
+		return shifts
+
+
+class Coulomb(Potential):
+	'''
+	 Calculations for the Coulomb energy contribution.
+	 Ewald summation method used for long range.
+	'''
+	def set_parameters(self, alpha, real_cut_off=4, recip_cut_off=4):
+		self.real_cut_off  = math.ceil(real_cut_off)
+		self.recip_cut_off = math.ceil(recip_cut_off)
+		self.alpha         = alpha
+
+	def get_reciprocal_vects(self):
+		'''
+		 Calculate reciprocal vectors
+		'''
+		rvects = np.zeros((3,3))
+		for i in np.arange(3):
+			rvects[i,] = 2*pi*np.cross( self.vects[(1+i)%3,], \
+									self.vects[(2+i)%3] ) / self.volume
+		return rvects
+
+	def get_charges_mult(self, index1, index2):
+		'''
+		 Find respective charges and
+		 return their product
+		'''
+		return ( self.charges[index1]*self.charges[index2] )
+
+	def calc_self(self, esum=[]):
+		'''
+		 Calculate self interaction term
+		'''
+		if esum == []:
+			esum = np.zeros((self.N, self.N))
+		for i in range(0, self.N):
+			esum[i, i] -= ( self.get_charges_mult(i, i) * ( self.alpha / math.sqrt(pi) ))
+		return esum
+
+	def calc_real(self, esum=[]):
+		'''
+		 Calculate short range
+		'''
+		if esum == []:
+			esum = np.zeros((self.N, self.N))
+		shifts = self.get_shifts( self.real_cut_off,self.vects )
+		for ioni in range(0, self.N): 
+			for ionj in range(ioni, self.N): 
+				if ioni != ionj: # skip in case it's the same atom in original unit cell
+					rij = np.linalg.norm(self.pos[ioni,] - self.pos[ionj,])
+					esum[ioni, ionj] += ( self.get_charges_mult(ioni,ionj) * \
+												 math.erfc( self.alpha*rij )/(2*rij) )
+				# take care of the rest lattice (+ Ln)
+				for shift in shifts:
+					rij = np.linalg.norm(self.pos[ioni,] + shift - self.pos[ionj,])
+					esum[ioni, ionj] += ( self.get_charges_mult(ioni,ionj) * \
+												math.erfc( self.alpha*rij )/(2*rij) )
+		return esum
+
+	def calc_recip(self, recip_vects, esum=[]):
+		'''
+		 Calculate long range
+		'''
+		if esum == []:
+			esum        = np.zeros((self.N, self.N))
+		shifts = self.get_shifts( self.recip_cut_off,recip_vects )
+		for ioni in range(0, self.N): 
+			for ionj in range(ioni, self.N): 
+
+				dist = self.pos[ionj,] - self.pos[ioni,] 
+				for k in shifts:
+					po = -np.dot(k,k)/(4*self.alpha**2)
+					numerator = 4 * pi * (math.exp(po)) * math.cos(np.dot(k, dist))
+					denominator = np.dot(k,k) * 2 * self.volume
+					esum[ioni, ionj] += (( self.get_charges_mult(ioni,ionj) ) * \
+																(numerator/denominator))
+		return esum
+
+	def calc_complete(self, esum):
+		'''
+		 Complete lower triangular matrix of monopole to monopole
+		'''
+		esum *= 14.399645351950543
+		for ioni in range(0, self.N):
+			for ionj in range(0, ioni):
+				esum[ioni, ionj] = esum[ionj, ioni]
+		return esum
+
+class Buckingham(Potential):
+	'''
+	 Calculations for the Buckingham energy contribution.
+	'''
+	def set_parameters(self, filename, real_cut_off=2):
+		'''
+		 Set atom_i-atom_j parameters as read from library file:
+		 - par: [A(eV), rho(Angstrom), C(eVAngstrom^6)]
+		 - lo : min radius (Angstrom)
+		 - hi : max radius (Angstrom)
+		'''
+		self.buck = {}
+		self.real_cut_off  = real_cut_off
+
+		try:
+			with open(filename,"r") as fin:
+				for line in fin:
+					line = line.split()
+					if ( len(line)<4 ):
+						continue
+					pair = (min(line[0],line[2]), max(line[0],line[2]))
+					self.buck[pair] = {}
+					self.buck[pair]['par'] = list(map(float, line[4:7]))
+					self.buck[pair]['lo'] = float(line[7])
+					self.buck[pair]['hi'] = float(line[-1])
+		except IOError:
+			print("No library file found.")
+
+	def get_cutoff(self, hi):
+		'''
+		 Find how many cells away to check
+		 using the minimum cell vector value
+		'''
+		cutoff = math.ceil(hi/min(self.vects[self.vects!=0]))
+		return cutoff
+
+	def calc_real(self, esum=0):
+		'''
+		 Interatomic potential
+		'''
+		chemical_symbols = self.atoms.get_chemical_symbols()
+		for ioni in range(self.N):
+			for ionj in range(self.N):
+				# Find the pair we are examining
+				pair = (min(chemical_symbols[ioni], chemical_symbols[ionj]), \
+								max(chemical_symbols[ioni], chemical_symbols[ionj]))
+				if (pair in self.buck):
+				# Pair of ions is listed in parameters file
+					A    = self.buck[pair]['par'][0]
+					rho  = self.buck[pair]['par'][1]
+					C    = self.buck[pair]['par'][2]
+
+					dist = np.linalg.norm(self.pos[ioni] - self.pos[ionj])
+					# Check if distance of ions allows interaction 					
+					if (dist < self.buck[pair]['hi']) & (ioni != ionj):
+						esum +=  A*math.exp(-1.0*dist/rho) - C/dist**6
+					
+					# Check interactions with neighbouring cells
+					cutoff = self.get_cutoff(self.buck[pair]['hi'])
+					shifts = self.get_shifts( cutoff,self.vects )
+					for shift in shifts:
+						dist = np.linalg.norm(self.pos[ioni] + shift - self.pos[ionj])
+						# Check if distance of ions allows interaction 					
+						if (dist < self.buck[pair]['hi']):
+							esum += A*math.exp(-1.0*dist/rho) - C/dist**6
+		return esum/2
